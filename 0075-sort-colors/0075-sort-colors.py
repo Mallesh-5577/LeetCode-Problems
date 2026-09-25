20c0790974447b89761338e8a740355f8e77cb90
Mallesh Mudavath <malleshmudavath12345@gmail.com>
2026-09-25 14:57:19 +0530

    Time: 3 ms (18.43%), Space: 12.5 MB (20.15%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -2,12 +2,11 @@ class Solution(object):
     def sortColors(self, nums):        
         n=len(nums)
 
-        for i in range(n-2,-1,-1):
-            swapped = False
-            for j in range(i+1):
-                if nums[j]>nums[j+1]:
-                    nums[j],nums[j+1]=nums[j+1],nums[j]
-                    swapped = True
-            if not swapped:
-                break
+        for i in range(0,n):
+            min_index = i
+            for j in range(i+1,n):
+                if nums[j]<nums[min_index]:
+                    min_index=j
+            nums[i],nums[min_index]=nums[min_index],nums[i]
+        
         return nums
