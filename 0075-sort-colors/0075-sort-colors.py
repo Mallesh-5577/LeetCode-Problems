2ab7f66fa3fcac690256dbac998b2c2a374245b9
Mallesh Mudavath <malleshmudavath12345@gmail.com>
2026-09-25 14:48:16 +0530

    Time: 7 ms (7.46%), Space: 12.3 MB (91.86%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,14 +1,13 @@
 class Solution(object):
-    def sortColors(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: None Do not return anything, modify nums in-place instead.
-        """
+    def sortColors(self, nums):        
+        n=len(nums)
 
-        for i in range(len(nums)):
-            for j in range(len(nums)-i-1):
+        for i in range(n-2,-1,-1):
+            swapped = False
+            for j in range(i+1):
                 if nums[j]>nums[j+1]:
                     nums[j],nums[j+1]=nums[j+1],nums[j]
-        
+                    swapped = True
+            if not swapped:
+                break
         return nums
-        
